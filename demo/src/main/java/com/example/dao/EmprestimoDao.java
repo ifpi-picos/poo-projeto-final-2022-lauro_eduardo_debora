7c4a0cb7eff62db2753fe79b66619628df4ab04d
deborah-andrade <deborahandraderibeiro44@gmail.com>
2023-01-09 19:15:27 -0300

    atualização do emprestimo

--- demo/src/main/java/com/example/dao/EmprestimoDao.java
+++ demo/src/main/java/com/example/dao/EmprestimoDao.java
@@ -1,5 +1,66 @@
 package com.example.dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.destino.Emprestimo;
+
 public class EmprestimoDao {
-   
+    Connection conexao;
+
+    public EmprestimoDao(){
+        conexao = ConexaoDao.getConexao();
+    }
+
+    public void adicionarEmprestimo(Emprestimo emprestimo){
+        try {
+            Statement stm = conexao.createStatement();
+            String sql = "insert into emprestimos (tempo_emp, valor_emp, data_emp, id_livro, cpf_usuario) values ('"+emprestimo.getTempoEmp()+"','"+emprestimo.getValorEmp()+"', '"+emprestimo.getDataEmp()+"','"+emprestimo.getLivroEmp()+"', '"+emprestimo.getUsuarioEmp()+"')";
+            stm.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean buscarEmprestimo(String cpf){
+
+        String sql = "select id_emprestimo from emprestimos where emprestimos.cpf_usuario = '"+cpf+"'";
+
+        System.out.println("CPF: \n" + cpf);
+
+        try { 
+            Statement stm = conexao.createStatement();
+            ResultSet result = stm.executeQuery(sql);
+
+            // System.out.println("result: " + result);
+
+            if(result.next()){
+                // System.out.println("Entrou no true");
+                return true;
+            }else{
+                // System.out.println("Entrou no false");
+                return false;
+            } 
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return false;
+        }
+    }
+
+    public void removerEmprestimo(String CPF_usuario){
+        try {
+            Statement stm = conexao.createStatement();
+            String sql = "delete from funcionarios where emprestimos.cpf_usuario = '"+CPF_usuario+"'";
+            stm.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- demo/src/main/java/com/example/visao/EmprestimoForm.java
+++ demo/src/main/java/com/example/visao/EmprestimoForm.java
@@ -1,6 +1,21 @@
 package com.example.visao;
 
+import java.sql.Date;
 
+import com.example.dao.EmprestimoDao;
+import com.example.destino.Emprestimo;
 
 public class EmprestimoForm {
+    public void cadastrarEmprestimo(Date data_emp, int id_livro, String CPF_usuario, float valor, int tempo_emp){
+        Emprestimo emprestimo = new Emprestimo(data_emp, id_livro, CPF_usuario, valor, tempo_emp);
+        new EmprestimoDao().adicionarEmprestimo(emprestimo);
+    }
+
+    public boolean encontrarEmprestimo(String cpf){
+        return new EmprestimoDao().buscarEmprestimo(cpf);
+    }
+
+    public void removerEmprestimo(String cpf){
+        new EmprestimoDao().removerEmprestimo(cpf);
+    }
 }
